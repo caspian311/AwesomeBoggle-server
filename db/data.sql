9bb13a3213839812423116fc9db4adf53d32a086
Matt Todd <matt.c.todd@gmail.com>
2018-11-24 07:12:32 -0600

    fixed available users logic and added more test data

--- db/data.sql
+++ db/data.sql
@@ -10,22 +10,31 @@ SET @user1 = LAST_INSERT_ID();
 INSERT INTO users (username, created_on) values ('abbi', NOW());
 SET @user2 = LAST_INSERT_ID();
 
+INSERT INTO users (username, created_on) values ('caleb', NOW());
+SET @user3 = LAST_INSERT_ID();
+
+INSERT INTO users (username, created_on) values ('aurelia', NOW());
+SET @user4 = LAST_INSERT_ID();
+
+INSERT INTO users (username, created_on) values ('peter', NOW());
+SET @user5 = LAST_INSERT_ID();
+
 INSERT INTO games (created_on, finished)
   values (@hour_ago, 1);
-SET @game_id = LAST_INSERT_ID();
+SET @game1 = LAST_INSERT_ID();
 
 INSERT INTO scores (user_id, game_id, score)
-  values (@user1, @game_id, 12);
+  values (@user1, @game1, 12);
 
 INSERT INTO scores (user_id, game_id, score)
-  values (@user2, @game_id, 20);
+  values (@user2, @game1, 20);
 
 INSERT INTO games (created_on, finished)
   values (NOW(), 0);
-SET @game_id = LAST_INSERT_ID();
+SET @game2 = LAST_INSERT_ID();
 
 INSERT INTO scores (user_id, game_id, score)
-  values (@user1, @game_id, 0);
+  values (@user3, @game2, 0);
 
 INSERT INTO scores (user_id, game_id, score)
-  values (@user2, @game_id, 0);
+  values (@user4, @game2, 0);
--- src/index.js
+++ src/index.js
@@ -22,13 +22,18 @@ conn.connect((err) => {
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: false}))
 
+
 app.get('/availableUsers', (req, res) => {
   let sql = `
     SELECT u.*
-    FROM users u, games g, scores s
-    WHERE s.game_id = g.id
+    FROM users u
+    WHERE u.id NOT IN (
+      SELECT u.id
+      FROM users u, games g, scores s
+      WHERE s.game_id = g.id
       AND s.user_id = u.id
       AND g.finished = 0
+    )
   `;
   conn.query(sql, (err, results) => {
     if (err) {
