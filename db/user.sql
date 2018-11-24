2f20599d5961ed918a57dfa14f4a899ff18a5d67
Matt Todd <matt.c.todd@gmail.com>
2018-11-23 18:28:54 -0600

    mysql8 is dumb

--- db/user.sql
+++ db/user.sql
@@ -1,5 +1,5 @@
 DROP USER IF EXISTS 'awesomeboggle'@'localhost';
 
-CREATE USER 'awesomeboggle'@'localhost' IDENTIFIED BY 'awesomeboggle';
+CREATE USER 'awesomeboggle'@'localhost' IDENTIFIED WITH mysql_native_password BY 'awesomeboggle';
 
 GRANT ALL PRIVILEGES ON awesomeboggle.* TO 'awesomeboggle'@'localhost';
