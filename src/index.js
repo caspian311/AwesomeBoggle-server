daf834947563e04e2e651f5dc8791cf63b2fb752
Matt Todd <matt.c.todd@gmail.com>
2018-11-24 08:18:55 -0600

    added complete game

--- src/index.js
+++ src/index.js
@@ -68,6 +68,50 @@ app.post('/joinGame', (req, res) => {
   });
 });
 
+app.put('/completeGame', (req, res) => {
+  let userId = req.body["score"]["userId"];
+  let gameId = req.body["score"]["gameId"];
+  let score = req.body["score"]["score"];
+
+  let updateScoreSql = `
+    UPDATE scores
+    SET score = ?
+    WHERE game_id = ?
+    AND user_id = ?
+  `;
+  conn.query(updateScoreSql, [ score, gameId, userId ], (err, results) => {
+    if (err) {
+      throw err;
+    }
+
+    let updateGameSql = `
+      UPDATE games
+      SET finished = 1
+      WHERE id = ?
+    `;
+    conn.query(updateGameSql, [ gameId ], (err, results) => {
+      if (err) {
+        throw err;
+      }
+
+      let getGameSql = `
+        SELECT g.id as gameId, u.id as userId, u.username as username, s.score as score
+        FROM games g, users u, scores s
+        WHERE g.id = s.game_id
+        AND u.id = s.user_id
+        AND g.id = ?
+      `;
+      conn.query(getGameSql, [ gameId ], (err, results) => {
+        if (err) {
+          throw err;
+        }
+
+        res.json(results)
+      });
+    });
+  });
+});
+
 let server = app.listen(8080, () => {
   let host = server.address().address;
   let port = server.address().port;
