239eb05e4cc819c67a19decec7655ebca1437caf
Matt Todd <matt.c.todd@gmail.com>
2018-12-09 09:01:19 -0600

    fixed issue of dropping same user for all environments all the time

--- db/user.sql
+++ db/user.sql
@@ -1,8 +1,8 @@
-DROP USER IF EXISTS 'awesomeboggle'@'localhost';
+-- DROP USER IF EXISTS 'awesomeboggle'@'localhost';
 
-CREATE USER 'awesomeboggle'@'localhost' IDENTIFIED WITH mysql_native_password BY 'awesomeboggle';
+CREATE USER IF NOT EXISTS 'awesomeboggle'@'localhost'
+  IDENTIFIED WITH mysql_native_password BY 'awesomeboggle';
 
 SET @grantstatement = CONCAT('GRANT ALL PRIVILEGES ON ', @db, '.* TO ''awesomeboggle''@''localhost''');
 PREPARE preparedgrant FROM @grantstatement;
 EXECUTE preparedgrant;
-
